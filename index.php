d8c6c2c596ac6a76934b8544aac880b86a4a1d5f
Алексей Мустафин <alexey.mustafin@ptnl.moscow>
2021-03-09 19:55:35 +0300

    Init commit

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+if (PHP_SAPI !== 'cli') {
+    die("Is cli-based application.");
+}
+
 spl_autoload_register(function ($class_name) {
     include "./Classes/" . $class_name . '.php';
 });
@@ -7,13 +11,9 @@ spl_autoload_register(function ($class_name) {
 use Utils\SortStation;
 use Utils\Polish;
 
-if (PHP_SAPI === 'cli') {
-    $SortStation = new SortStation(fgets(STDIN));
-    $lexems = $SortStation
-        ->parseLexems()
-        ->getLexems();
-    echo Polish::Parse($lexems) . "\n";
-    echo eval("return " . $SortStation->getFormula() . ";");
-} else {
-    die("Is cli-based application.");
-}
+$SortStation = new SortStation(fgets(STDIN));
+$lexems = $SortStation
+    ->parseLexems()
+    ->getLexems();
+echo Polish::Parse($lexems) . "\n";
+echo eval("return " . $SortStation->getFormula() . ";");
